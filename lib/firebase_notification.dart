da51bbcefc022de595928f035cf7c386f8812e17
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2024-04-30 08:22:03 -0400

    refactored get token

--- lib/firebase_notification.dart
+++ lib/firebase_notification.dart
@@ -3,9 +3,20 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class FirebaseNotification {
   final _firebaseMessage = FirebaseMessaging.instance;
 
-  Future<String?> requestNotifications() async {
-    await _firebaseMessage.requestPermission();
-    final token = await _firebaseMessage.getToken();
-    print("token $token");
+  Future<String?> getTokenFire() async {
+    return await _firebaseMessage.getToken();
+  }
+
+  Future<void> requestNotifications() async {
+    NotificationSettings settings = await _firebaseMessage.requestPermission();
+    String? token = await getTokenFire();
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
   }
 }
